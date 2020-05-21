4d93e9fcca522dd93baa563eb6621a8194b72e44
Konrad Siek <konrad.siek@gmail.com>
2020-05-21 12:13:45 +0200

    Hand-written ant script.

--- src/cz/cvut/fit/prl/sudoku/Sudoku.java
+++ src/cz/cvut/fit/prl/sudoku/Sudoku.java
@@ -43,7 +43,7 @@ public class Sudoku {
              *   - have the board be displayed as a matrix
              *   - should append to the previous boards, but show the last n solutions along side their boards
              */
-            printer.print(solved.toCSV(), "string");
+            printer.print(solved == null ? null : solved.toCSV(), "string");
             //printer.print("solution", solved.toCSV(), "csv");
 
             long stop = System.currentTimeMillis();
@@ -254,93 +254,93 @@ public class Sudoku {
         board[1].set2D(8,8,0);
 
         board[2].set2D(0,0,0);
-        board[2].set2D(0,1,5);
-        board[2].set2D(0,2,1);
-        board[2].set2D(0,3,2);
-        board[2].set2D(0,4,7);
+        board[2].set2D(0,1,0);
+        board[2].set2D(0,2,0);
+        board[2].set2D(0,3,0);
+        board[2].set2D(0,4,0);
         board[2].set2D(0,5,0);
-        board[2].set2D(0,6,0);
-        board[2].set2D(0,7,0);
-        board[2].set2D(0,8,4);
+        board[2].set2D(0,6,1);
+        board[2].set2D(0,7,4);
+        board[2].set2D(0,8,0);
 
         board[2].set2D(1,0,0);
         board[2].set2D(1,1,0);
-        board[2].set2D(1,2,0);
+        board[2].set2D(1,2,2);
         board[2].set2D(1,3,0);
         board[2].set2D(1,4,0);
-        board[2].set2D(1,5,3);
+        board[2].set2D(1,5,6);
         board[2].set2D(1,6,0);
         board[2].set2D(1,7,0);
-        board[2].set2D(1,8,5);
+        board[2].set2D(1,8,8);
 
         board[2].set2D(2,0,0);
-        board[2].set2D(2,1,4);
+        board[2].set2D(2,1,0);
         board[2].set2D(2,2,0);
-        board[2].set2D(2,3,0);
-        board[2].set2D(2,4,0);
-        board[2].set2D(2,5,0);
+        board[2].set2D(2,3,1);
+        board[2].set2D(2,4,8);
+        board[2].set2D(2,5,4);
         board[2].set2D(2,6,0);
-        board[2].set2D(2,7,8);
-        board[2].set2D(2,8,0);
+        board[2].set2D(2,7,0);
+        board[2].set2D(2,8,6);
 
-        board[2].set2D(3,0,0);
-        board[2].set2D(3,1,0);
+        board[2].set2D(3,0,7);
+        board[2].set2D(3,1,5);
         board[2].set2D(3,2,0);
-        board[2].set2D(3,3,0);
-        board[2].set2D(3,4,8);
-        board[2].set2D(3,5,0);
+        board[2].set2D(3,3,6);
+        board[2].set2D(3,4,0);
+        board[2].set2D(3,5,8);
         board[2].set2D(3,6,0);
-        board[2].set2D(3,7,1);
-        board[2].set2D(3,8,6);
+        board[2].set2D(3,7,9);
+        board[2].set2D(3,8,1);
 
-        board[2].set2D(4,0,0);
-        board[2].set2D(4,1,0);
-        board[2].set2D(4,2,2);
+        board[2].set2D(4,0,8);
+        board[2].set2D(4,1,3);
+        board[2].set2D(4,2,0);
         board[2].set2D(4,3,5);
         board[2].set2D(4,4,0);
-        board[2].set2D(4,5,4);
-        board[2].set2D(4,6,7);
-        board[2].set2D(4,7,0);
-        board[2].set2D(4,8,0);
+        board[2].set2D(4,5,9);
+        board[2].set2D(4,6,0);
+        board[2].set2D(4,7,6);
+        board[2].set2D(4,8,5);
 
-        board[2].set2D(5,0,0);
-        board[2].set2D(5,1,0);
+        board[2].set2D(5,0,8);
+        board[2].set2D(5,1,3);
         board[2].set2D(5,2,0);
-        board[2].set2D(5,3,0);
-        board[2].set2D(5,4,8);
-        board[2].set2D(5,5,0);
+        board[2].set2D(5,3,5);
+        board[2].set2D(5,4,0);
+        board[2].set2D(5,5,9);
         board[2].set2D(5,6,0);
-        board[2].set2D(5,7,1);
-        board[2].set2D(5,8,6);
+        board[2].set2D(5,7,6);
+        board[2].set2D(5,8,2);
 
-        board[2].set2D(6,0,0);
+        board[2].set2D(6,0,1);
         board[2].set2D(6,1,0);
-        board[2].set2D(6,2,2);
-        board[2].set2D(6,3,5);
-        board[2].set2D(6,4,0);
-        board[2].set2D(6,5,4);
-        board[2].set2D(6,6,7);
+        board[2].set2D(6,2,0);
+        board[2].set2D(6,3,4);
+        board[2].set2D(6,4,5);
+        board[2].set2D(6,5,7);
+        board[2].set2D(6,6,0);
         board[2].set2D(6,7,0);
         board[2].set2D(6,8,0);
 
-        board[2].set2D(7,0,0);
+        board[2].set2D(7,0,3);
         board[2].set2D(7,1,0);
         board[2].set2D(7,2,0);
-        board[2].set2D(7,3,0);
+        board[2].set2D(7,3,8);
         board[2].set2D(7,4,0);
-        board[2].set2D(7,5,9);
-        board[2].set2D(7,6,0);
-        board[2].set2D(7,7,5);
+        board[2].set2D(7,5,0);
+        board[2].set2D(7,6,6);
+        board[2].set2D(7,7,0);
         board[2].set2D(7,8,0);
 
-        board[2].set2D(8,0,8);
-        board[2].set2D(8,1,0);
-        board[2].set2D(8,2,5);
-        board[2].set2D(8,3,3);
-        board[2].set2D(8,4,6);
+        board[2].set2D(8,0,0);
+        board[2].set2D(8,1,7);
+        board[2].set2D(8,2,8);
+        board[2].set2D(8,3,0);
+        board[2].set2D(8,4,0);
         board[2].set2D(8,5,0);
-        board[2].set2D(8,6,4);
-        board[2].set2D(8,7,2);
+        board[2].set2D(8,6,0);
+        board[2].set2D(8,7,0);
         board[2].set2D(8,8,0);
 
         return board;
